e2b74656df877ade77e7c4a3b4572a1db04e760a
Jannik Buschke <jannik.buschke@gmail.com>
2020-05-23 16:53:20 +0200

    add 'silence' prototype

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -1,37 +1,93 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import { Formik } from "formik"
-import { Input, SubmitButton, Form } from "formik-antd"
-import { notification } from "antd"
-import { AntDesignOutlined } from "@ant-design/icons"
+import { SubmitButton, Form, Radio, Slider } from "formik-antd"
+import { RadioGroup } from "formik-antd/es/form-items"
+import {
+  BackgroundContainer,
+  SilencePulse,
+  SilencedBackgroundContainer,
+} from "./silence"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+  const [silence, setSilence] = React.useState(false)
+  const Background = silence ? SilencedBackgroundContainer : BackgroundContainer
 
-      <div style={{ background: "white", padding: "20px" }}>
-        <Formik
-          initialValues={{ field1: "hello world" }}
-          onSubmit={(values, f) => {
-            notification.info({ message: values.field1 })
-            f.setSubmitting(false)
-          }}
-        >
-          <Form>
-            <Input name="field1" />
-            <SubmitButton
-              style={{ marginTop: 10 }}
-              icon={<AntDesignOutlined />}
-            >
-              Submit
-            </SubmitButton>
-          </Form>
-        </Formik>
-      </div>
+  React.useEffect(() => {
+    if (silence) {
+      setTimeout(() => {
+        setSilence(false)
+      }, 20000)
+    }
+  }, [silence])
+  return (
+    <div>
+      <Formik
+        initialValues={{ volume: 50, remember: null, remembrance: null }}
+        onSubmit={(values, f) => {
+          f.setSubmitting(false)
+          setSilence(values.remember === "beschweigen")
+        }}
+      >
+        {(f) => (
+          <Background>
+            <div className="App">
+              <div>ERINNERUNG</div>
+              <div>GENERIEREN</div>
+              <div>LEBENDIG</div>
+              <div>GEDENKEN</div>
+              <br />
+              <br />
+              <div style={{ padding: "20px" }}>
+                <Form colon={false}>
+                  <RadioGroup name="remember" size="large">
+                    <Radio.Button name="remember" value="trauern">
+                      Trauern
+                    </Radio.Button>
+                    <Radio.Button name="remember" value="gedenken">
+                      Gedenken
+                    </Radio.Button>
+                    <Radio.Button name="remember" value="bereuen">
+                      Bereuen
+                    </Radio.Button>
+                    <Radio.Button name="remember" value="verachten">
+                      Verachten
+                    </Radio.Button>
+                    <Radio.Button name="remember" value="beschweigen">
+                      Beschweigen
+                    </Radio.Button>
+                  </RadioGroup>
+                  <Form.Item name="volume">
+                    <Slider
+                      disabled={f.values.remember === "beschweigen"}
+                      name="volume"
+                      marks={{ 0: "Leise", 100: "Laut" }}
+                      tooltipVisible={false}
+                    />
+                  </Form.Item>
+                  <RadioGroup
+                    name="remembrance"
+                    size="large"
+                    disabled={f.values.remember === "beschweigen"}
+                  >
+                    <Radio.Button name="remembrance" value="gedenken">
+                      Gedenken
+                    </Radio.Button>
+                    <Radio.Button name="remembrance" value="ehren">
+                      Ehren
+                    </Radio.Button>
+                    <Radio.Button name="remembrance" value="mahnen">
+                      Mahnen
+                    </Radio.Button>
+                  </RadioGroup>
+                  <SubmitButton style={{ marginTop: 10 }}>Starten</SubmitButton>
+                </Form>
+              </div>
+            </div>
+          </Background>
+        )}
+      </Formik>
+      {silence && <SilencePulse />}
     </div>
   )
 }
---
+++ app/web/src/silence.tsx
@@ -0,0 +1,88 @@
+import * as React from "react"
+import styled, { keyframes } from "styled-components"
+
+const color = "rgba(10,10,10,0.4)"
+const color2 = "rgba(10,10,10,0)"
+
+const animation = keyframes`
+    0% {
+      box-shadow: 0 0 0 0 ${color};
+    }
+    100% {
+        box-shadow: 0 0 0 80px ${color2};
+    }
+`
+
+export function SilencePulse() {
+  return (
+    <SilenceContainer>
+      <Silence />
+    </SilenceContainer>
+  )
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 0.5;
+  }
+`
+
+const SilenceContainer = styled.div`
+  opacity: 0;
+  animation: ${fadeIn} 9s forwards 1s;
+`
+
+export const Silence = styled.div`
+  position: absolute;
+  top: calc(50vh - 60px);
+  left: calc(50vw - 60px);
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background: black;
+  box-shadow: 0 0 0 ${color};
+  animation: ${animation} 3.5s infinite;
+`
+
+const blurIn = keyframes`
+  from {
+    background-color: white;
+    filter: blur(0px);
+    opacity: 1;
+  }
+
+  to {
+    filter: blur(55px);
+    background-color: #000000e3;
+    opacity: 0.9;
+  }
+`
+
+export const SilencedBackgroundContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${blurIn}};
+  animation-duration: 5s;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
+  animation-delay: .5s;
+`
+export const BackgroundContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
