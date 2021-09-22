0e4597b9f4418ed22d0eddbc7507af0f800291a8
JulaJulaJolo <90446218+JulaJulaJolo@users.noreply.github.com>
2021-09-22 12:11:29 +0200

    initial commit (#2)

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -18,14 +18,54 @@ export function History() {
       <br />
       <br />
       <Space direction="vertical" size="large">
-        <Link>Initiative Dessauer Ufer</Link>
-        <Link>KZ Gedenkstätte Neuengamme Dessauer Ufer</Link>
-        <Link>Hamburg Veddel Frauen</Link>
-        <Link>Hamburg Veddel Männer</Link>
         <Link>
-          Berichte und Biografien von Hédi Fried, Livia Fränkel, Edith Kraus,
-          Dagmar Lieblová, Zuzana Rùžièková, Jean Le Bris u. w.
+          <a href="https://initiativedessauerufer.noblogs.org/" target="_blank">
+            Initiative Dessauer Ufer
+          </a>
         </Link>
+        <Link>
+          <a href="https://www.kz-gedenkstaette-neuengamme.de/" target="_blank">
+            KZ Gedenkstätte Neuengamme
+          </a>
+        </Link>
+        <Link>
+          <a
+            href="https://www.kz-gedenkstaette-neuengamme.de/geschichte/kz-aussenlager/aussenlagerliste/hamburg-veddel-frauen/"
+            target="_blank"
+          >
+            Hamburg Veddel Frauen
+          </a>
+        </Link>
+        <Link>
+          <a
+            href="https://www.kz-gedenkstaette-neuengamme.de/geschichte/kz-aussenlager/aussenlagerliste/hamburg-veddel-maenner/"
+            target="_blank"
+          >
+            Hamburg Veddel Männer
+          </a>
+        </Link>
+
+        <Link>
+          <a
+            href="https://sites.google.com/view/lagerhaus-g-hamburg/lagerhaus-g-schon-mal-geh%C3%B6rt?"
+            target="_blank"
+          >
+            Curating the Invisible: Lagerhaus G
+          </a>
+        </Link>
+
+        <Link>
+          <a
+            href="https://www.kz-gedenkstaette-neuengamme.de/vorbereitung/biografie_hedi_szmuk.html"
+            target="_blank"
+          >
+            Biografien von Hédi Fried und Livia Fränkel
+          </a>
+        </Link>
+        <p>
+          Links zu weiteren Berichten und Biografien (Edith Kraus, Dagmar
+          Lieblová, Zuzana Rùžièková, Jean Le Bris) folgen.
+        </p>
       </Space>
       <br />
       <br />
@@ -35,6 +75,8 @@ export function History() {
 
 const Title = styled.h1`
   color: white;
+  margin: 0;
+  font-size: calc(12px + 1.8vmin);
 `
 
 const Link = styled.a`
